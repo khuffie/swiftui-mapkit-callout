45f6b2f071ab61253ecfea71065158e13619ce60
Ahmed El-Khuffash <ahmed@el-khuffash.com>
2020-05-31 15:51:33 -0400

    Initial Commit

---
+++ SwiftUI View Inside a MapKit Callout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI View Inside a MapKit Callout
+//
+//  Created by Ahmed El-Khuffash on 2020-05-31.
+//  Copyright © 2020 Ahmed El-Khuffash. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
